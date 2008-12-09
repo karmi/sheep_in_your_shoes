e88540be6e8b7ebd2da65c54d8e95f33a2950c9f
Karel Minarik <karmi@karmi.cz>
2008-12-09 19:42:16 +0100

    Let's add lots of sheep|

--- sheep_in_your_shoes.rb
+++ sheep_in_your_shoes.rb
@@ -1,21 +1,28 @@
 Shoes.app do
 
+  NUM_SHEEPS = 25
+
   # The Pasture
   background gradient( "#fff", "#00ff05")
   border black, :strokewidth => 6
 
-  # The Sheep
+  x, y = 10, self.height - 10
+
+  # Sheeps
+  @sheeps = []
   fill "#fff"
   stroke "#000"
   strokewidth 3
-  @sheep = oval(0, 0, 15, 15)
-
-  x, y = self.width / 2, self.height
+  1.upto NUM_SHEEPS do |i|
+    sheep = oval(0, 0, 15, 15)
+    sheep.move x + i * 15, y
+    @sheeps << sheep
+  end
 
   animate(20) do
     # x += 1
-    y -= 1
-    @sheep.move x.to_i, y.to_i
+    y -= 5
+    @sheeps[ rand NUM_SHEEPS ].move x.to_i, y.to_i
   end
 
 end
